516e99f4a2f6b8bb3839e9af83e2c090680455ba
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-17 10:30:36 +0100

    test with matplotlb

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 print("Output of test")
 
+# Generate a dummy plot to test if matplotlib is working
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+plt.figure()
+plt.plot(x, y)
+plt.show()
