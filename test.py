6af0dbffe1c3f26b2c9a22844acf11a822475089
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-17 10:36:06 +0100

    savefig for testing

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ print("Output of test")
 # Generate a dummy plot to test if matplotlib is working
 x = np.linspace(0, 2*np.pi, 100)
 y = np.sin(x)
+
+# Plot and save figure
 plt.figure()
 plt.plot(x, y)
-plt.show()
+plt.savefig('test.png')
+
