45ec26b6324cb99de90fd7dbf2da2eec3e1b789e
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-18 18:47:20 +0100

    try just 2 cores

--- submit_test.sh
+++ submit_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH -n 3            # Request 3 cores
+#SBATCH -n 2            # Request 2 cores
 #SBATCH -t 0-00:01:00   # Request 1 minute (which provides a buffer for the 5-second expected runtime)
 #SBATCH -p compute      # Use the "compute" partition
 #SBATCH -J test_mpi     # Job name
