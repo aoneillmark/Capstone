03bd83831c118a347c4fde0a3c756ad2aa86c3a0
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-19 18:47:10 +0100

    pickle file format, running test

--- VOTPP folder/VOTPP_convergence_runner.py
+++ VOTPP folder/VOTPP_convergence_runner.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import pickle
 
 # Start timer
 start = time.time()
@@ -14,7 +15,36 @@ size = MPI.COMM_WORLD.Get_size()
 
 print("Rank: {}".format(rank))
 
-concentration_list = [0, 0.02, 0.05, 0.1, 0.2, 0.5]
+
+# Runner to converge parameters
+def runner(concentration_value, 
+           changing_variable, variable_values, 
+           bath_parameters, simulator_parameters, calc_parameters,):
+    
+    changing_invalue = simulator_parameters[changing_variable] # Save this for later
+    bath_parameters['concentration'] = concentration_value # Set concentration value
+
+    simulator = VOTPP_class(**bath_parameters) # Set up bath and atoms
+    sim = simulator.setup_simulator(**simulator_parameters) # Set up simulator
+
+    # ls = []
+    results = {}
+    for v in variable_values: # Iterate through variable values (e.g. order = 1, 2, 3)
+        setattr(simulator, changing_variable, v) # Set the variable in the simulator to the value (e.g. simulator['order'] = 1)
+        l = sim.compute(**calc_parameters)
+        
+        # Printing for progress check, listing the variable and value
+        print("{}: {}".format(changing_variable, v))
+        
+        results[v] = pd.DataFrame([l.real], columns=calc_parameters['timespace']).T
+        # ls.append(l.real)
+
+    simulator_parameters[changing_variable] = changing_invalue
+    # ls = pd.DataFrame(ls, columns=calc_parameters['timespace'], index=variable_values).T
+    return results
+
+# concentration_list = [0, 0.02, 0.05, 0.1, 0.2, 0.5]
+concentration_list = [0.02, 0.05,]
 order_list = [1, 2, 3] 
 r_bath_list = [40, 80, 160, 220]
 r_dipole_list = [20, 40, 60, 100, 140, 180]
@@ -25,7 +55,7 @@ default_calc_parameters = {
     'timespace': np.linspace(0, 1e-1, 201),
     'method': 'cce',
     'pulses': 1,
-    'nbstates': 4, #!
+    'nbstates': 250, #!
     'quantity': 'coherence',
     'parallel': True,
     'parallel_states': True,
@@ -39,10 +69,10 @@ default_bath_parameters = {
 }
 
 default_simulator_parameters = { ########## These should be greater when simulating with HPC
-    'order': 2, #!
-    'r_bath': 20, #!
-    'r_dipole': 10, #!
-    'pulses': 2, # N pulses in CPMG sequence
+    'order': 3, #!
+    'r_bath': 80, #!
+    'r_dipole': 60, #!
+    'pulses': 1, # N pulses in CPMG sequence
     'magnetic_field': [0, 0, 10000], # Magnetic field in Gauss
 }
 
@@ -58,50 +88,30 @@ default_simulator_parameters = { ########## These should be greater when simulat
 # if rank == 0:
 #     simulator.plot_results(default_calc_parameters['timespace'], result)
 
-
-# Runner to converge parameters
-def runner(concentration_value, 
-           changing_variable, variable_values, 
-           bath_parameters, simulator_parameters, calc_parameters,):
-    
-    changing_invalue = simulator_parameters[changing_variable] # Save this for later
-    bath_parameters['concentration'] = concentration_value # Set concentration value
-
-    simulator = VOTPP_class(**bath_parameters) # Set up bath and atoms
-    sim = simulator.setup_simulator(**simulator_parameters) # Set up simulator
-
-    ls = []
-    for v in variable_values: # Iterate through variable values (e.g. order = 1, 2, 3)
-        setattr(simulator, changing_variable, v) # Set the variable in the simulator to the value (e.g. simulator['order'] = 1)
-        l = sim.compute(**calc_parameters)
-        
-        # Printing for progress check, listing the variable and value
-        print("{}: {}".format(changing_variable, v))
-        
-        ls.append(l.real)
-
-    simulator_parameters[changing_variable] = changing_invalue
-    ls = pd.DataFrame(ls, columns=calc_parameters['timespace'], index=variable_values).T
-    return ls
-
-order_results = runner(concentration_value=0,
-                       changing_variable='order', variable_values=[1,2,3],
-                       bath_parameters=default_bath_parameters, simulator_parameters=default_simulator_parameters, calc_parameters=default_calc_parameters,)
+order_results = {}
+for conc in concentration_list:
+    order_results[conc] = runner(concentration_value=conc,
+                        changing_variable='order', variable_values=order_list,
+                        bath_parameters=default_bath_parameters, simulator_parameters=default_simulator_parameters, calc_parameters=default_calc_parameters,)
 
 # Print time taken
 end = time.time()
 print("Time taken: {} seconds".format(end - start))
 
+path = "VOTPP folder/Results/"
+
 # Save this data to an external file
 if rank == 0:
-    order_results.to_csv('order_results.csv')
+    # Saving results
+    with open((str(path) + 'order_results.pkl'), 'wb') as f:
+        pickle.dump(order_results, f)
 
-if rank == 0:
-    # Plot these results
-    plt.figure()
-    plt.plot(order_results)
-    plt.legend(order_results.columns)
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Coherence')
-    plt.title('Convergence test for order')
-    plt.show()
+# if rank == 0:
+#     # Plot these results
+#     plt.figure()
+#     plt.plot(order_results)
+#     plt.legend(order_results.columns)
+#     plt.xlabel('Time (ms)')
+#     plt.ylabel('Coherence')
+#     plt.title('Convergence test for order')
+#     plt.show()
