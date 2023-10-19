7b612e722f4c61be7dcb2003d60a3e6f43ad8269
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-19 18:48:28 +0100

    changed for test with pickle

--- submit_test.sh
+++ submit_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #SBATCH -n 4            # Request 4 cores
-#SBATCH -t 0-00:05:00   # Request 1 minute (which provides a buffer for the 5-second expected runtime)
+#SBATCH -t 0-00:10:00   # Request 1 minute (which provides a buffer for the 5-second expected runtime)
 #SBATCH -p compute      # Use the "compute" partition
 #SBATCH -J test_mpi     # Job name
 #SBATCH -o /path/to/Capstone/slurm_outputs/slurm-%j.out
@@ -12,4 +12,4 @@ source ~/miniconda3/bin/activate Capstone_conda_env2
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 # Run the MPI script using slurm # mpirun python test.py
-mpirun -np 4 python "Cathal Hogan code/VOTPP_convergence_runner.py"
+mpirun -np 4 python "VOTPP folder/VOTPP_convergence_runner.py"
