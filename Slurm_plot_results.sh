42ef563938bdf24465dd1bc1df826503153c2113
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-20 17:22:48 +0100

    Updated slurm script to reflect directories

--- Slurm_plot_results.sh
+++ Slurm_plot_results.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#SBATCH -n 1            # Request 4 cores
-#SBATCH -t 0-00:10:00   # Request 1 minute (which provides a buffer for the 5-second expected runtime)
+#SBATCH -n 1            # Request 1 cores
+#SBATCH -t 0-00:10:00   # Request 10 minutes
 #SBATCH -p compute      # Use the "compute" partition
-#SBATCH -J test_mpi     # Job name
+#SBATCH -J Plotting     # Job name
+#SBATCH -o Capstone/VOTPP folder/Slurm_plotting/slurm-%j.out
+#SBATCH -e Capstone/VOTPP folder/Slurm_plotting/slurm-%j.err
 
 # Activate the Conda environment
 source ~/miniconda3/bin/activate Capstone_conda_env2
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-# Run the MPI script using slurm # mpirun python test.py
-# mpirun -np 16 python "VOTPP folder/VOTPP_convergence_runner.py"
+# Run the MPI script using slurm 
 mpirun -np 1 python "VOTPP folder/Results/VOTPP_results_plotter.py"
-# mpirun -np 4 python "test.py"
--- Slurm_run_convergence.sh
+++ Slurm_run_convergence.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#SBATCH -n 16            # Request 4 cores
-#SBATCH -t 0-00:10:00   # Request 1 minute (which provides a buffer for the 5-second expected runtime)
-#SBATCH -p compute      # Use the "compute" partition
-#SBATCH -J test_mpi     # Job name
+#SBATCH -n 16              # Request 16 cores
+#SBATCH -t 0-00:10:00      # Request 10 minutes
+#SBATCH -p compute         # Use the "compute" partition
+#SBATCH -J Convergence     # Job name
+#SBATCH -o Capstone/VOTPP folder/Slurm_convergence/slurm-%j.out
+#SBATCH -e Capstone/VOTPP folder/Slurm_convergence/slurm-%j.err
 
 # Activate the Conda environment
 source ~/miniconda3/bin/activate Capstone_conda_env2
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-# Run the MPI script using slurm # mpirun python test.py
+# Run the MPI script using slurm 
 mpirun -np 16 python "VOTPP folder/VOTPP_convergence_runner.py"
-# mpirun -np 1 python "VOTPP folder/Results/VOTPP_results_plotter.py"
-# mpirun -np 4 python "test.py"
