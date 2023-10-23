c5200cdfe1583648f6bd598c4232e8a82e184f7f
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-23 16:22:43 +0100

    Set up for 0% convergence test

--- VOTPP folder/VOTPP_convergence_runner.py
+++ VOTPP folder/VOTPP_convergence_runner.py
@@ -44,18 +44,23 @@ def runner(concentration_value,
     return results
 
 # concentration_list = [0, 0.02, 0.05, 0.1, 0.2, 0.5]
-concentration_list = [0.02, 0.05,]
-order_list = [1, 2, 3] 
-r_bath_list = [40, 80, 160, 220]
-r_dipole_list = [20, 40, 60, 100, 140, 180]
+# concentration_list = [0.02, 0.05,]
+concentration_list = [0]
+order_list = [1, 2, 3, 4] 
+# r_bath_list = [40, 80, 160, 220]
+# r_dipole_list = [20, 40, 60, 100, 140, 180]
+r_bath_list = [10,20,30]
+r_dipole_list = [8,10,12]
+# cell_size_list = [60,100,200]
 cell_size_list = [60,100,200]
-nbstates_list = [32, 64, 128, 256]
+# nbstates_list = [32, 64, 128, 256]
+nbstates_list = [16,32,64]
 
 default_calc_parameters = {
     'timespace': np.linspace(0, 1e-2, 201),
     'method': 'cce',
     'pulses': 1,
-    'nbstates': 512, #!
+    'nbstates': 32, #!
     'quantity': 'coherence',
     'parallel': True,
     'parallel_states': True,
@@ -64,16 +69,17 @@ default_calc_parameters = {
 default_bath_parameters = {
     'concentration': 0, #!
     'cell_size': 100, #!
-    'displacement': 3.75, # Angstrom
+    'displacement': 0, # Angstrom
     'seed': 8000
 }
 
 default_simulator_parameters = { ########## These should be greater when simulating with HPC
     'order': 3, #!
-    'r_bath': 200, #!
-    'r_dipole': 150, #!
+    'r_bath': 20, #!
+    'r_dipole': 10, #!
     'pulses': 1, # N pulses in CPMG sequence
-    'magnetic_field': [0, 0, 10000], # Magnetic field in Gauss
+    # 'magnetic_field': [0, 0, 10000], # Magnetic field in Gauss
+    'magnetic_field': [1000, 0, 0], # Magnetic field in Gauss
 }
 
 
@@ -94,6 +100,18 @@ for conc in concentration_list:
                         changing_variable='order', variable_values=order_list,
                         bath_parameters=default_bath_parameters, simulator_parameters=default_simulator_parameters, calc_parameters=default_calc_parameters,)
 
+r_bath_results = {}
+for conc in concentration_list:
+    r_bath_results[conc] = runner(concentration_value=conc,
+                        changing_variable='r_bath', variable_values=order_list,
+                        bath_parameters=default_bath_parameters, simulator_parameters=default_simulator_parameters, calc_parameters=default_calc_parameters,)
+
+r_dipole_results = {}
+for conc in concentration_list:
+    r_dipole_results[conc] = runner(concentration_value=conc,
+                        changing_variable='r_dipole', variable_values=order_list,
+                        bath_parameters=default_bath_parameters, simulator_parameters=default_simulator_parameters, calc_parameters=default_calc_parameters,)
+
 # Print time taken
 end = time.time()
 print("Time taken: {} seconds".format(end - start))
@@ -105,6 +123,14 @@ if rank == 0:
     # Saving results
     with open((str(path) + 'order_results.pkl'), 'wb') as f:
         pickle.dump(order_results, f)
+    
+    with open((str(path) + 'r_bath_results.pkl'), 'wb') as f:
+        pickle.dump(r_bath_results, f)
+    
+    with open((str(path) + 'r_dipole_results.pkl'), 'wb') as f:
+        pickle.dump(r_dipole_results, f)
+
+
 
 # if rank == 0:
 #     # Plot these results
