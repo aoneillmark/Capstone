1cf226850565e5265e2cc7dddc41675ace5f9cc4
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-24 16:17:49 +0100

    2 hours just in case

--- Slurm_run_convergence.sh
+++ Slurm_run_convergence.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #SBATCH -n 16              # Request 16 cores
-#SBATCH -t 0-01:00:00      # Request 60 minutes
+#SBATCH -t 0-02:00:00      # Request 60 minutes
 #SBATCH -p compute         # Use the "compute" partition
 #SBATCH -J Convergence     # Job name
 #SBATCH -o "Capstone/VOTPP folder/Slurm_convergence/slurm-%j.out"
