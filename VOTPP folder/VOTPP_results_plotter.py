578767497d20feb0010ef0306d09a94b740b6b75
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-24 17:01:58 +0100

    added tuple handling in the plotter

--- VOTPP folder/VOTPP_results_plotter.py
+++ VOTPP folder/VOTPP_results_plotter.py
@@ -8,8 +8,14 @@ with open((str(path) + 'order_results.pkl') , 'rb') as f:
     loaded_results = pickle.load(f)
 
 concentration_to_plot = 0.05
-for order_val, df in loaded_results[concentration_to_plot].items():
-    plt.plot(df.index, df[0], label=f"Order {order_val}")
+for v_key, df in loaded_results[concentration_to_plot].items():
+    # Check if the key is a tuple and handle the label accordingly
+    if isinstance(v_key, tuple):
+        label_str = ', '.join(map(str, v_key))
+        label = f"Field: ({label_str})"
+    else:
+        label = f"Order {v_key}"
+    plt.plot(df.index, df[0], label=label)
 
 plt.legend()
 plt.xlabel('Time')
@@ -21,5 +27,5 @@ image_path = "VOTPP folder/Results/Plots/"
 output_filename = f"{image_path}Concentration_{concentration_to_plot}.png"
 plt.savefig(output_filename, dpi=300)  # dpi is dots per inch (resolution). You can adjust as needed.
 
-# If you still want to show the plot, you can uncomment the next line
-plt.show()
+# If you want to show the plot, you can uncomment the next line
+# plt.show()
