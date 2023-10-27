3dcc6de807c373fa3aed6dbd413fe6bf06b1046b
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-27 17:02:44 +0100

    25 cores

--- Slurm_cathal_hogan.sh
+++ Slurm_cathal_hogan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH -n 128              # Request 128 cores
+#SBATCH -n 25              # Request 128 cores
 #SBATCH -t 0-12:05:00      # Request 12 hours and 5 minutes
 #SBATCH -p compute         # Use the "compute" partition
 #SBATCH -J CombinedJob     # Job name
@@ -12,7 +12,7 @@ source ~/miniconda3/bin/activate Capstone_conda_env2
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 # Run the MPI script for simulation using slurm 
-mpirun -np 128 python "Cathal Hogan code/tbs n-n T2 MULTIPLE SPINS.py"
+mpirun -np 25 python "Cathal Hogan code/tbs n-n T2 MULTIPLE SPINS.py"
 
 # Wait for the simulation to complete before starting plotting
 wait
