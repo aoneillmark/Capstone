4b3f56b986042da64a174d938e78706bcc82e007
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-10-29 16:31:15 +0000

    rename, save T2 and B to separate pickle

--- VOTPP folder/VOTPP_results_and_fit_plotter.py
+++ VOTPP folder/VOTPP_results_and_fit_plotter.py
@@ -5,7 +5,7 @@ import pickle
 import os
 
 def coherence_time_func(time, beta, T2):
-    y = np.exp(-(((time)/T2)**beta))
+    y = np.exp(-(((time)/T2)**beta)) # Is there a scaling issue here?
     return y
 
 def load_data_from_file(path, pickle_filename):
@@ -26,7 +26,7 @@ def plot_combined(loaded_results, variable_name, image_path):
         plt.plot(df.index, df[0], label=label_str)
 
     plt.title(f"All {variable_name.capitalize()} Results")
-    plt.xlabel('Time')
+    plt.xlabel('Time (ms)')
     plt.ylabel('Coherence')
     plt.legend()
     plt.tight_layout()
@@ -35,15 +35,31 @@ def plot_combined(loaded_results, variable_name, image_path):
     plt.savefig(output_filename, dpi=300)
     plt.show()
 
-def plot_individual_with_fit(loaded_results, variable_name, image_path):
+def save_fit_results(fit_results, result_name, save_path):
+    """
+    Save the fit results to a pickle file.
+    """
+    output_filename = os.path.join(save_path, "Fits/", f"{result_name}_fit_results.pkl")
+    with open(output_filename, 'wb') as f:
+        pickle.dump(fit_results, f)
+    print("Saved fit results to file:")
+    print(output_filename)
+
+def plot_individual_with_fit(loaded_results, variable_name, image_path, pickle_path):
+    fit_results = {}  # Dictionary to store fit parameters for all magnetic fields
+
     for v_key, df in loaded_results[0].items():
         plt.figure(figsize=(10,6))
-        ydata = df[df.index <= 0.1][0]
+        ydata = df[0].abs()  # Taking absolute values
         label_str = ', '.join(map(str, v_key)) if isinstance(v_key, tuple) else f"Value {v_key}"
         
         try:
-            params, _ = curve_fit(coherence_time_func, df.index, ydata, maxfev=5000, bounds=([0, -np.inf], [np.inf, np.inf]))
+            params, _ = curve_fit(coherence_time_func, df.index, ydata, maxfev=5000, bounds=([0, -np.inf], [np.inf, np.inf]), p0=[1,0.025])
             beta_fit, T2_fit = params
+            
+            # Save the results in the dictionary
+            fit_results[label_str] = {'beta': beta_fit, 'T2': T2_fit} 
+            
             plt.plot(df.index, ydata, 'o', label=f'Data for {label_str}')
             plt.plot(df.index, coherence_time_func(df.index, *params), '--', 
                      label=f'Fit: Beta={beta_fit:.3f}, T2={T2_fit:.3f}')
@@ -51,7 +67,7 @@ def plot_individual_with_fit(loaded_results, variable_name, image_path):
             print(f"Fit for label {label_str} failed: {e}")
 
         plt.title(f"{variable_name.capitalize()} Result for {label_str}")
-        plt.xlabel('Time')
+        plt.xlabel('Time (ms)')
         plt.ylabel('Coherence')
         plt.legend()
         plt.tight_layout()
@@ -60,19 +76,21 @@ def plot_individual_with_fit(loaded_results, variable_name, image_path):
         plt.savefig(output_filename, dpi=300)
         plt.show()
 
+    save_fit_results(fit_results, variable_name, pickle_path)  # Save the fit results once all fits are done
+
 def plot_from_file(pickle_filenames):
-    path = "VOTPP folder/Results/Pickle files/"
+    pickle_path = "VOTPP folder/Results/Pickle files/"
     image_path = "VOTPP folder/Results/Plots/"
 
     for pickle_filename in pickle_filenames:
         variable_name = pickle_filename.split('_results.pkl')[0]
-        loaded_results = load_data_from_file(path, pickle_filename)
+        loaded_results = load_data_from_file(pickle_path, pickle_filename)
         
         # Plot all results combined without fit
         plot_combined(loaded_results, variable_name, image_path)
         
         # Plot individual results with fit
-        plot_individual_with_fit(loaded_results, variable_name, image_path)
+        plot_individual_with_fit(loaded_results, variable_name, image_path, pickle_path)
 
 # Provide the filenames you want to process as a list
 # plot_from_file(['magnetic_results.pkl', 'r_bath_results.pkl', 'r_dipole_results.pkl'])
