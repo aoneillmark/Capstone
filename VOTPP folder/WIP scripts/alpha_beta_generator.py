f191eb6318c774dd46f418986fe5d2796d867c90
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-01 11:42:54 +0000

    corrected with Valerio's advice. Final is correct

--- VOTPP folder/WIP scripts/alpha_beta_generator.py
+++ VOTPP folder/WIP scripts/alpha_beta_generator.py
@@ -2,7 +2,8 @@ import numpy as np
 
 def nuclear_vector(m_I):
     # Define the basis vectors for nuclear spin
-    nuclear_basis = [7/2, 5/2, 3/2, 1/2, -1/2, -3/2, -5/2, -7/2]
+    # nuclear_basis = [7/2, 5/2, 3/2, 1/2, -1/2, -3/2, -5/2, -7/2]
+    nuclear_basis = [-7/2, -5/2, -3/2, -1/2, 1/2, 3/2, 5/2, 7/2]
     # nuclear_basis = [7/2, 5/2, 3/2, 1/2, -7/2, -5/2, -3/2, -1/2]
     
     # Initialize the vector as a zero vector of length 8
@@ -15,7 +16,7 @@ def nuclear_vector(m_I):
 
 def electron_vector(m_S):
     # Define the basis vectors for electron spin
-    electron_basis = [1/2, -1/2]
+    electron_basis = [-1/2, 1/2]
     
     # Initialize the vector as a zero vector of length 2
     vector = [0] * len(electron_basis)
@@ -31,10 +32,10 @@ def kronecker(m_I, m_S):
     return np.kron(nuclear_vec, electron_vec)
 
 # Test
-m_I1, m_S1 = 1/2, -1/2
+m_I1, m_S1 = -7/2, -1/2
 alpha = (kronecker(m_I1, m_S1))
 
-m_I2, m_S2 = 3/2, -1/2
+m_I2, m_S2 = -7/2, 1/2
 beta = (kronecker(m_I2, m_S2))
 
 print("Kronecker product:")
@@ -46,8 +47,8 @@ print("Beta: ", list(beta))
 
 def state_to_index(m_s, m_e):
     # Lists of states for nuclear and electron spins
-    nuclear_states = [7/2, 5/2, 3/2, 1/2, -1/2, -3/2, -5/2, -7/2]
-    electron_states = [1/2, -1/2]
+    nuclear_states = [-7/2, -5/2, -3/2, -1/2, 1/2, 3/2, 5/2, 7/2] 
+    electron_states = [-1/2, 1/2]
 
     # Find the position of the states in the arrays
     nuclear_index = nuclear_states.index(m_s)
@@ -71,8 +72,8 @@ def get_alpha_beta(initial_state, final_state):
     return alpha_array, beta_array
 
 # Example usage:
-initial_state = (1/2, -1/2)
-final_state = (3/2, -1/2)
+initial_state = (-7/2, -1/2)
+final_state = (7/2, 1/2)
 alpha1, beta1 = get_alpha_beta(initial_state, final_state)
 
 print("Using initial ordering")
@@ -84,12 +85,12 @@ print(f"Beta:  {beta1}")
 
 def state_to_index2(m_s, m_e):
     combined_states = [
-        # +1/2
-        (7/2, 1/2), (5/2, 1/2), (3/2, 1/2), (1/2, 1/2),
-        (-1/2, 1/2), (-3/2, 1/2), (-5/2, 1/2), (-7/2, 1/2),
         # -1/2
-        (7/2, -1/2), (5/2, -1/2), (3/2, -1/2), (1/2, -1/2),
-        (-1/2, -1/2), (-3/2, -1/2), (-5/2, -1/2), (-7/2, -1/2)
+        (-7/2, -1/2), (-5/2, -1/2), (-3/2, -1/2), (-1/2, -1/2),
+        (1/2, -1/2), (3/2, -1/2), (5/2, -1/2), (7/2, -1/2),
+        # +1/2
+        (-7/2, 1/2), (-5/2, 1/2), (-3/2, 1/2), (-1/2, 1/2),
+        (1/2, 1/2), (3/2, 1/2), (5/2, 1/2), (7/2, 1/2),
     ]
 
     return combined_states.index((m_s, m_e))
@@ -108,15 +109,17 @@ def get_alpha_beta2(initial_state, final_state):
     return alpha_array, beta_array
 
 # Example usage:
-initial_state = (1/2, -1/2)
-final_state = (3/2, -1/2)
+# initial_state = (1/2, -1/2)
+# final_state = (3/2, -1/2)
+initial_state = (-7/2, -1/2)
+final_state = (-7/2, 1/2)
 alpha, beta = get_alpha_beta2(initial_state, final_state)
 print("Using other ordering method:")
 print(f"Alpha: {alpha}")
 print(f"Beta:  {beta}")
 
-print("Reverse the lists of alpha and beta")
-alpha.reverse()
-beta.reverse()
-print(f"Alpha: {alpha}")
-print(f"Beta:  {beta}")
+# print("Reverse the lists of alpha and beta")
+# alpha.reverse()
+# beta.reverse()
+# print(f"Alpha: {alpha}")
+# print(f"Beta:  {beta}")
