1cf7bf8500000581a973c5eacecdca6fd9e92f59
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-03 11:48:24 +0000

    128 core

--- Slurm_combined.sh
+++ Slurm_combined.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH -n 256              # Request 128 cores
+#SBATCH -n 128              # Request 128 cores
 #SBATCH -t 0-05:00:00      # Request 1 hours and 0 minutes
 #SBATCH -p compute         # Use the "compute" partition
 #SBATCH -J CombinedJob     # Job name
@@ -12,7 +12,7 @@ source ~/miniconda3/bin/activate Capstone_conda_env2
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 # Run the MPI script for simulation using slurm 
-mpirun -np 256 python "VOTPP folder/VOTPP_convergence_runner.py"
+mpirun -np 128 python "VOTPP folder/VOTPP_convergence_runner.py"
 
 # Wait for the simulation to complete before starting plotting
 wait
