a0f4c26b2714b16820eed5217d501e494a512c80
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-03 11:52:27 +0000

    Try again, order=3 valerio alphabeta

--- VOTPP folder/VOTPP_results_and_fit_plotter.py
+++ VOTPP folder/VOTPP_results_and_fit_plotter.py
@@ -93,7 +93,7 @@ def plot_from_file(pickle_filenames):
         plot_individual_with_fit(loaded_results, variable_name, image_path, pickle_path)
 
 # Provide the filenames you want to process as a list
-# plot_from_file(['magnetic_results.pkl',])
+plot_from_file(['magnetic_results.pkl',])
 # plot_from_file(['magnetic_results.pkl', 'r_bath_results.pkl', 'r_dipole_results.pkl'])
-plot_from_file(['order_results.pkl'])
+# plot_from_file(['order_results.pkl'])
 # plot_from_file(['cell_size_results.pkl'])
