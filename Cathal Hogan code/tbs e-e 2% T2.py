04ab433460a164de52e187c0e26eaaf24767dc14
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-06 18:06:40 +0000

    misc changes

--- Cathal Hogan code/tbs e-e 2% T2.py
+++ Cathal Hogan code/tbs e-e 2% T2.py
@@ -46,22 +46,19 @@ cen = pc.CenterArray(size=1, position=qpos, spin=1/2,
                     D=0, gyro=-17608.59705, alpha=[0,1], beta=[1,0],)
 
 #parameters
-nb = 512 # 240
+nb = 6 # 250
 ts = np.linspace(0, 5e-3, 201) #expected T2 of 3.3e-2
 cell = 200 #cell size 
 
 
 #dictionary of parameters
-parameters = {
-    'order':2, # CCE order
-    'r_bath':100,  # Size of the bath in A
-    'r_dipole':60,  # Cutoff of pairwise clusters in A
-    'pulses' :1, # N pulses in CPMG sequence
-    'magnetic_field': [0,0,10000] #set to 1T
-}
-
-print("A processor is running this")
-
+parameters = dict(
+    order=3, # CCE order
+    r_bath=100,  # Size of the bath in A
+    r_dipole=60,  # Cutoff of pairwise clusters in A
+    pulses = 1, # N pulses in CPMG sequence
+    magnetic_field=[0,0,10000] #set to 1T
+) 
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -115,32 +112,32 @@ for i in range(no_sims):
 
 if rank == 0:
     print("Ranks! ", rank, size)
-    # #use all 50 sims to calculate average curve 
-    # avg = np.zeros(201) #creates array of 0s same length as ts
-    # for i in range(201): 
-    #     for j in range(no_sims):
-    #         avg[i] += sims[j][i]   
-    # avg = avg/no_sims
+    #use all 50 sims to calculate average curve 
+    avg = np.zeros(201) #creates array of 0s same length as ts
+    for i in range(201): 
+        for j in range(no_sims):
+            avg[i] += sims[j][i]   
+    avg = avg/no_sims
 
-    # #plot the average coherence curve 
-    # plt.plot(ts, avg)
-    # plt.xlabel('Time [ms]')
-    # plt.ylabel('Coherence')
-    # plt.title('Averaged Coherence Curve e-e 2%')
-    # plt.show()
+    #plot the average coherence curve 
+    plt.plot(ts, avg)
+    plt.xlabel('Time [ms]')
+    plt.ylabel('Coherence')
+    plt.title('Averaged Coherence Curve e-e 2%')
+    plt.show()
 
-    # #fit coherence curve 
-    # p_guess= [0.02, 2] #guessing T2 and the power respectively 
-    # par, cov = curve_fit(fit, ts, avg, p0=p_guess,) #find best fitting parameters and covariance matrix
-    # err = np.sqrt(np.diag(cov)) #calculating the errors based on the fit 
-    # print('params =', par) # print params
+    #fit coherence curve 
+    p_guess= [0.02, 2] #guessing T2 and the power respectively 
+    par, cov = curve_fit(fit, ts, avg, p0=p_guess,) #find best fitting parameters and covariance matrix
+    err = np.sqrt(np.diag(cov)) #calculating the errors based on the fit 
+    print('params =', par) # print params
 
-    # #plot fitted curve 
-    # label = 'Fit - T2=' + str(round(par[0]*1e3,3)) + ' \u00B1 ' + str(round(err[0]*1e3,3)) + ' us'
-    # plt.plot(ts, avg, label='average for 50')
-    # plt.plot(ts, fit(ts, *par), 'r-', label=label)
-    # plt.xlabel('Time [ms]')
-    # plt.ylabel('Coherence')
-    # plt.title('Averaged Coherence Curve e-e 2%')
-    # plt.legend()
-    # plt.show()
+    #plot fitted curve 
+    label = 'Fit - T2=' + str(round(par[0]*1e3,3)) + ' \u00B1 ' + str(round(err[0]*1e3,3)) + ' us'
+    plt.plot(ts, avg, label='average for 50')
+    plt.plot(ts, fit(ts, *par), 'r-', label=label)
+    plt.xlabel('Time [ms]')
+    plt.ylabel('Coherence')
+    plt.title('Averaged Coherence Curve e-e 2%')
+    plt.legend()
+    plt.show()
