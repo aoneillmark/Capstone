a5af52c699c3df48013d50a405af1f1baf9384fc
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-06 18:07:49 +0000

    don't plot anything for the time being

--- VOTPP folder/VOTPP_results_and_fit_plotter.py
+++ VOTPP folder/VOTPP_results_and_fit_plotter.py
@@ -107,7 +107,9 @@ def plot_from_file(pickle_filenames):
 
 # plot_from_file(['magnetic_results_e_n.pkl',])
 # plot_from_file(['magnetic_results_n_n.pkl',])
-plot_from_file(['magnetic_results_[n-e]_e.pkl',])
+# plot_from_file(['magnetic_results_[n-e]_e.pkl',])
+# plot_from_file(['magnetic_results_e_e.pkl',])
+# plot_from_file(['magnetic_results_n_e.pkl',])
 
 # plot_from_file(['alphabeta_results_0.pkl', 'alphabeta_results_1.pkl', 'alphabeta_results_2.pkl', 'alphabeta_results_3.pkl', ])
 # plot_from_file(['alphabeta_results.pkl',])
