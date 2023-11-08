ec43c1619f2fe94a9e00eb057453eaabb75c316a
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-08 15:38:09 +0000

    [e-n]-n, n2n, r_X=60,20 with INTEGER alphabeta

--- VOTPP folder/VOTPP_convergence_runner_copy.py
+++ VOTPP folder/VOTPP_convergence_runner_copy.py
@@ -117,7 +117,7 @@ default_bath_parameters = {
 
 default_simulator_parameters = { ########## These should be greater when simulating with HPC
     'order': 2, #!
-    'r_bath': 35, #35
+    'r_bath': 60, #35
     'r_dipole': 20, #20
     # 'pulses': 1, # N pulses in CPMG sequence (=1 is Hahn-echo, =0 is free induction decay)
     # 'pulses': [pc.Pulse('x', 2*(np.pi)/3)], # Paper defines a Hahn-echo pulse sequence with 2pi/3 pulses?
