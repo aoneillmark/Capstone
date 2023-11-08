d3ba8298de731caa229e6a2cc7ae3e2bb972a47f
FIRST_NAME LAST_NAME <aoneillmark@gmail.com>
2023-11-08 11:26:37 +0000

    changed params

--- VOTPP folder/VOTPP_convergence_runner_[n-e]_e_N_trans.py
+++ VOTPP folder/VOTPP_convergence_runner_[n-e]_e_N_trans.py
@@ -99,7 +99,7 @@ nbstates_list = [128,]
 
 default_calc_parameters = {
     # 'timespace': np.linspace(0, 7e-2, 201),
-    'timespace': np.linspace(0, 1e-1, 201), # 7e-2
+    'timespace': np.linspace(0, 1e-2, 201), # 7e-2
     # 'timespace': np.linspace(0, , 2), # 7e-2
     'method': 'cce',
     'pulses': [pc.Pulse('x', np.pi)], # Paper defines a Hahn-echo pulse sequence with 2pi/3 pulses?
@@ -116,9 +116,9 @@ default_bath_parameters = {
 }
 
 default_simulator_parameters = { ########## These should be greater when simulating with HPC
-    'order': 3, #!
-    'r_bath': 200, #35
-    'r_dipole': 100, #20
+    'order': 2, #!
+    'r_bath': 200, #200, #35
+    'r_dipole': 100, #100, #20
     # 'pulses': 1, # N pulses in CPMG sequence (=1 is Hahn-echo, =0 is free induction decay)
     # 'pulses': [pc.Pulse('x', 2*(np.pi)/3)], # Paper defines a Hahn-echo pulse sequence with 2pi/3 pulses?
     # 'pulses': [pc.Pulse('x', np.pi), pc.Pulse('y', np.pi)],
