6c4b696d90365eedfcdee0843ee03e7c5a18cf39
Mark O'Neill <oneilm25@tcd.ie>
2023-12-29 17:26:54 +0000

    Handle NaNs better

--- VOTPP folder/VOTPP_T2_vs_percentages_plotter.py
+++ VOTPP folder/VOTPP_T2_vs_percentages_plotter.py
@@ -2,42 +2,55 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import os
+import pandas as pd
 
-# Define the curve fitting function, e.g., an exponential decay for T2
+# Define the curve fitting function
 def coherence_time_func(time, beta, T2):
-    y = np.exp(-(((2*time)/T2)**beta)) # Is there a scaling issue here?
-    return y
+    return np.exp(-(((2*time)/T2)**beta))
 
-concentration_list = [0.02,0.05,0.1,0.3]
+concentration_list = [0.02, 0.05, 0.1, 0.3]
 T2_values = []
 path = "VOTPP folder/Results/Pickle files/"
 
-plt.figure(figsize=(10, 6))
-
 for conc in concentration_list:
-    conc_percentage = conc*100
-
-    # Load results
-    with open(f'{path}results_conc_{conc}.pkl', 'rb') as f:
+    conc_percentage = conc * 100
+    with open(f'{path}results_combined_conc_{conc}.pkl', 'rb') as f:
         data = pickle.load(f)
 
     for key, df in data.items():
+        plt.figure(figsize=(10, 6))
+        coherence = df[0]  # Assuming df[0] is your data column
         time = df.index
-        coherence = abs(df[0])  # Access the first (and only) column by index
 
-        # Fit curve and extract T2
+        # Convert coherence values to their absolute values
+        coherence = np.abs(coherence)
+
+        # Replace '--' with 0 and convert to float
+        coherence = pd.Series(coherence).replace('--', 0).astype(float)
+
+        # Replace NaN and infinite values with zero
+        coherence = np.nan_to_num(coherence)
+
+        # Curve fitting and plotting logic
         try:
-            popt, _ =  curve_fit(coherence_time_func, time, abs(coherence), maxfev=5000, bounds=([0, -np.inf], [np.inf, np.inf]), p0=[1,0.00001])
-            T2 = popt[0]
-            T2_values.append(T2)
+            popt, _ = curve_fit(coherence_time_func, time, coherence, maxfev=5000, bounds=([0, -np.inf], [np.inf, np.inf]), p0=[1, 0.1])
+            beta_fit, T2_fit = popt
+            T2_values.append(T2_fit)
 
-            # Plot raw data
-            plt.plot(time, coherence, label=f'Conc {conc_percentage:.0f}% - Data')
-
-            # Plot fitted curve
-            plt.plot(time, coherence_time_func(time, *popt), linestyle='--', label=f'Conc {conc_percentage:.0f}% - Fit: $T_2$={T2:.2f} ms')
+            plt.plot(time, coherence, 'o', label=f'Conc {conc_percentage:.0f}% - Data')
+            plt.plot(time, coherence_time_func(time, *popt), '--', label=f'Fit: Beta={beta_fit:.3f}, $T_2$={T2_fit:.3f} ms')
         except RuntimeError as e:
             print(f"Fit failed for concentration {conc}: {e}")
+
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Coherence')
+        plt.title(f'Coherence vs Time with Fitted Curves for Conc {conc_percentage:.0f}%')
+        plt.legend()
+        plt.tight_layout()
+        output_filename = os.path.join(path, f"coherence_conc_{conc_percentage}_with_fit.png")
+        plt.savefig(output_filename, dpi=300)
+        plt.show()
 
 plt.xlabel('Time (ms)')
 plt.ylabel('Coherence')
@@ -55,7 +68,7 @@ plt.xlabel('Concentration of electron spins (%)')
 plt.ylabel(f'$T_2$ (ms)')
 plt.title(f'$T_2$ vs Concentration')
 plt.tight_layout()
-plt.savefig(f'{path}T2_vs_concentration_(E_trans).png', dpi=300)
+plt.savefig(f'{path}T2_vs_concentration_(combined).png', dpi=300)
 plt.show()
 
 # Save T2 values with respective concentrations to a pickle file for later use
