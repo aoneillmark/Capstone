37b4ee6aad628d58e073212a37493b8fdf29f1b2
Mark O'Neill <oneilm25@tcd.ie>
2024-01-01 20:45:45 +0000

    Product rule gets T2 = 88 ms!!!

--- VOTPP folder/VOTPP_product_rule.py
+++ VOTPP folder/VOTPP_product_rule.py
@@ -11,10 +11,12 @@ timespace = np.linspace(0, 1, 201)
 
 # Series parameters
 series_params = [
-    # {"beta": 0.503, "T2": 0.125},  # Series 1
-    # {"beta": 2.152, "T2": 0.611}   # Series 2
-    {"beta": 2.185, "T2": 0.249},  # Series 1
-    {"beta": 3.539, "T2": 1.028}   # Series 2
+    {"beta": 0.537, "T2": 0.110},  # Series 1
+    {"beta": 2.152, "T2": 0.611},   # Series 2
+    # {"beta": 2.185, "T2": 0.249},  # Series 1
+    # {"beta": 3.539, "T2": 1.028},   # Series 2
+    {"beta": 0.221, "T2": 144881},  # Series 3
+    {"beta": 0.213, "T2": 241023},  # Series 4
 ]
 
 # Calculate coherence for each series and store it
@@ -22,6 +24,14 @@ coherences = []
 for params in series_params:
     coherence = coherence_time_func(timespace, params["beta"], params["T2"])
     coherences.append(coherence)
+
+
+
+################################################
+# Plotting
+################################################
+image_path = "VOTPP folder/Results/Plots/"
+################################################
 
 # First plot: Plot each coherence curve
 plt.figure(figsize=(10, 6))
@@ -35,6 +45,7 @@ plt.xlabel("Time")
 plt.ylabel("Coherence")
 plt.legend()
 plt.tight_layout()
+plt.savefig(image_path + "coherence_vs_time_for_each_series.png", dpi=300)
 plt.show()
 
 # Second plot: Product of coherences
@@ -56,4 +67,5 @@ plt.xlabel("Time")
 plt.ylabel("Product of Coherence")
 plt.legend()
 plt.tight_layout()
+plt.savefig(image_path + "product_of_coherences_with_fit.png", dpi=300)
 plt.show()
