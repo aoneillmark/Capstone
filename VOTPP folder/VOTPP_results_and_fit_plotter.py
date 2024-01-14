ab6bb150604439ba0c255abea6a8c021e22e22cd
Mark O'Neill <oneilm25@tcd.ie>
2024-01-14 19:19:02 +0000

    Respects new file structure, improved look of plots, works for all filesets

--- VOTPP folder/VOTPP_results_and_fit_plotter.py
+++ VOTPP folder/VOTPP_results_and_fit_plotter.py
@@ -42,19 +42,18 @@ def check_pickle_structure(pickle_path, pickle_filename):
             for sub_key, df in data.items():
                 print(f"    Sub_key: {sub_key}, Type: {type(df)}, Structure: {type(df[0]) if isinstance(df, (list, tuple)) else 'N/A'}")
 
-def load_data_from_file(path, pickle_filename):
+def load_data_from_file(pickle_filename):
+    path = "VOTPP folder/Results/Pickle files 2/Simulation results/"
     with open(os.path.join(path, pickle_filename), 'rb') as f:
         loaded_results = pickle.load(f)
     
-    # Print the entire dictionary of loaded results
-    print(loaded_results.keys())
-
     # Convert all coherence values to their absolute values
     for key in loaded_results:
         for sub_key in loaded_results[key]:
             loaded_results[key][sub_key][0] = np.abs(loaded_results[key][sub_key][0])
     
     return loaded_results
+
 
 def print_series_before_averaging(loaded_results, filename="full_series_data.txt"):
     with open(filename, 'w') as file:
@@ -91,20 +90,20 @@ def average_results_by_seed(loaded_results, debug=False):
 
 
 
-def plot_combined(loaded_results, variable_name, image_path, ylim=None):
+def plot_combined(loaded_results, variable_name, image_path, ylim=None, show=False):
     colors = get_colorblind_friendly_colors()  # Get colorblind-friendly colors
-    plt.figure(figsize=(10,6))
+    plt.figure(figsize=(8,4))
     
     color_index = 0  # Initialize a separate color index
     for outer_key, data in loaded_results.items():
         for v_key, df in data.items():
             color = colors[color_index % len(colors)]  # Cycle through colors using color_index
             label_str = ', '.join(map(str, v_key)) if isinstance(v_key, tuple) else f"Value {v_key}"
-            plt.plot(df.index, df[0], label=label_str, color=color)
+            plt.plot(df.index, df[0], label=f"B_0 = {v_key[2]}", color=color)
             color_index += 1  # Increment color_index for each v_key
 
     plt.title(f"All {variable_name.capitalize()} Results")
-    plt.xlabel('Time (ms)')
+    plt.xlabel(r'2$\tau$ (ms)')
     plt.ylabel('Coherence')
     plt.legend()
     plt.tight_layout()
@@ -112,49 +111,50 @@ def plot_combined(loaded_results, variable_name, image_path, ylim=None):
     
     output_filename = os.path.join(image_path, f"{variable_name}_all_results.png")
     plt.savefig(output_filename, dpi=300)
-    plt.show()
+    if show == True:
+        plt.show()
+    else:
+        plt.close()
 
-def save_fit_results(fit_results, result_name, save_path):
+def save_fit_results(fit_results, result_name):
     """
     Save the fit results to a pickle file.
     """
-    output_filename = os.path.join(save_path, "Fits/", f"{result_name}_fit_results.pkl")
+    save_path = "VOTPP folder/Results/Pickle files 2/Fits/"
+    output_filename = os.path.join(save_path, f"{result_name}_fit_results.pkl")
     with open(output_filename, 'wb') as f:
         pickle.dump(fit_results, f)
-    print("Saved fit results to file:")
-    print(output_filename)
+    print("Saved fit results to file:", output_filename)
 
-def plot_individual_with_fit(loaded_results, variable_name, image_path, pickle_path, data_range=slice(None), ylim=None):
+
+def plot_individual_with_fit(loaded_results, variable_name, image_path, data_range=slice(None), ylim=None, show=False):
     fit_results = {}
 
     for outer_key in loaded_results.keys():
         for v_key, df in loaded_results[outer_key].items():
-            plt.figure(figsize=(10, 6))
+            plt.figure(figsize=(8, 4))
             ydata_series = pd.Series(df[0].iloc[data_range]).replace('--', np.nan).astype(float)
-
-            # Create a mask for valid data points (not NaN and finite)
             valid_mask = ~ydata_series.isna() & np.isfinite(ydata_series)
-
-            # Data for plotting and fitting
             ydata_plot = ydata_series[valid_mask]
             time_data = df.index[data_range][valid_mask]
 
             label_str = ', '.join(map(str, v_key)) if isinstance(v_key, tuple) else f"Value {v_key}"
 
             try:
-                params, _ = curve_fit(coherence_time_func, time_data, ydata_plot, maxfev=5000, bounds=([0, -np.inf], [np.inf, np.inf]), p0=[1, 0.060])
+                params, cov = curve_fit(coherence_time_func, time_data, ydata_plot, maxfev=5000, bounds=([0, -np.inf], [np.inf, np.inf]), p0=[1, 0.060])
                 beta_fit, T2_fit = params
+                beta_err, T2_err = np.sqrt(np.diag(cov))  # Standard deviation (error) of the fit parameters
 
                 fit_key = (outer_key,) + v_key if isinstance(v_key, tuple) else (outer_key, v_key)
-                fit_results[fit_key] = {'beta': beta_fit, 'T2': T2_fit}
+                fit_results[fit_key] = {'beta': beta_fit, 'T2': T2_fit, 'beta_err': beta_err, 'T2_err': T2_err}
 
-                plt.plot(time_data, ydata_plot, 'o', label=f'Data for {label_str}')
-                plt.plot(time_data, coherence_time_func(time_data, *params), '--', label=f'Fit: Beta={beta_fit:.3f}, T2={T2_fit:.3f}')
+                plt.plot(time_data, ydata_plot, 'o', label=f'Data for B_0 = {v_key[2]} G')
+                plt.plot(time_data, coherence_time_func(time_data, *params), '--', label=f'Fit: T_2 = {T2_fit:.3f}±{T2_err:.3f} ms')
             except RuntimeError as e:
                 print(f"Fit for label {label_str} failed: {e}")
 
             plt.title(f"{variable_name.capitalize()} Result for {label_str}")
-            plt.xlabel('Time (ms)')
+            plt.xlabel(r'2$\tau$ (ms)')
             plt.ylabel('Coherence')
             plt.legend()
             plt.tight_layout()
@@ -162,14 +162,15 @@ def plot_individual_with_fit(loaded_results, variable_name, image_path, pickle_p
 
             output_filename = os.path.join(image_path, f"{variable_name}_{label_str}_with_fit.png")
             plt.savefig(output_filename, dpi=300)
-            plt.show()
+            if show == True:
+                plt.show()
+            else:
+                plt.close()
 
-    save_fit_results(fit_results, variable_name, pickle_path)
+    save_fit_results(fit_results, variable_name,)
 
 
-    save_fit_results(fit_results, variable_name, pickle_path)
-
-def plot_individual_with_fit_average(averaged_results, variable_name, image_path, pickle_path, data_range=slice(None), ylim=None):
+def plot_individual_with_fit_average(averaged_results, variable_name, image_path, pickle_path, data_range=slice(None), ylim=None, show=False):
     fit_results = {}  # Dictionary to store fit parameters for all results
     
     for sub_key, series in averaged_results.items():  # Now iterating over averaged results
@@ -202,7 +203,10 @@ def plot_individual_with_fit_average(averaged_results, variable_name, image_path
         
         output_filename = os.path.join(image_path, f"{variable_name}_{label_str}_with_fit.png")
         plt.savefig(output_filename, dpi=300)
-        plt.show()
+        if show == True:
+            plt.show()
+        else:
+            plt.close()
 
     save_fit_results(fit_results, variable_name, pickle_path)
 
@@ -228,26 +232,24 @@ def plot_combined_average(averaged_results, variable_name, image_path, ylim=None
 
 
 def plot_from_file(pickle_filenames, data_range=slice(None), ylim=None):
-    pickle_path = "VOTPP folder/Results/Pickle files/"
-    image_path = "VOTPP folder/Results/Plots/"
+    image_path = "VOTPP folder/Results/Plots 2/"
 
     for pickle_filename in pickle_filenames:
         variable_name = pickle_filename.split('_results.pkl')[0]
-        loaded_results = load_data_from_file(pickle_path, pickle_filename)
-        
+        loaded_results = load_data_from_file(pickle_filename)
+
         # Plot all results combined without fit
         plot_combined(loaded_results, variable_name, image_path, ylim=ylim)
         
         # Plot individual results with fit
-        plot_individual_with_fit(loaded_results, variable_name, image_path, pickle_path, data_range=data_range, ylim=ylim)
+        plot_individual_with_fit(loaded_results, variable_name, image_path, data_range=data_range, ylim=ylim)
 
 def plot_from_file_average(pickle_filenames, data_range=slice(None), ylim=None):
-    pickle_path = "VOTPP folder/Results/Pickle files/"
-    image_path = "VOTPP folder/Results/Plots/"
+    image_path = "VOTPP folder/Results/Plots 2/"
 
     for pickle_filename in pickle_filenames:
         variable_name = pickle_filename.split('_results.pkl')[0]
-        loaded_results = load_data_from_file(pickle_path, pickle_filename)
+        loaded_results = load_data_from_file(pickle_filename)
 
         # Average results by seed
         averaged_results = average_results_by_seed(loaded_results, debug=False)
@@ -278,6 +280,9 @@ def plot_from_file_average(pickle_filenames, data_range=slice(None), ylim=None):
 # plot_from_file(['[n-e]-(e).pkl',],ylim=(-0.01,None))#, data_range=slice(0, 225), )
 # plot_from_file(['[n-e]-(n).pkl',])
 
+AB_list = ['AB1', 'AB2', 'AB3', 'AB4', 'AB5', 'AB6', 'AB7']
+for AB in AB_list:
+    plot_from_file([f'[n-e]-(e)_{AB}.pkl',], ylim=(-0.01,None))
 # plot_from_file(['[n-e]-(e)_AB1.pkl',])
 # plot_from_file(['[n-e]-(e)_AB2.pkl',])
 # plot_from_file(['[n-e]-(e)_AB3.pkl',])
@@ -287,7 +292,10 @@ def plot_from_file_average(pickle_filenames, data_range=slice(None), ylim=None):
 # plot_from_file(['[n-e]-(e)_AB7.pkl',])
 
 bath_type_list = ['C', 'N', 'H']
+AB_list = ['AB1', 'AB2', 'AB3', 'AB4', 'AB5', 'AB6', 'AB7']
 for bath_type in bath_type_list:
+    for AB in AB_list:
+        plot_from_file([f'[n-e]-(n)_{bath_type}_{AB}.pkl',], ylim=(-0.01,None))
     # plot_from_file([f'[n-e]-(n)_{bath_type}_AB1.pkl',], ylim=(-0.01,None))
     # plot_from_file([f'[n-e]-(n)_{bath_type}_AB2.pkl',], ylim=(-0.01,None))
     # plot_from_file([f'[n-e]-(n)_{bath_type}_AB3.pkl',], ylim=(-0.01,None))
